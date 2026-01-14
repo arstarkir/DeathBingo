318e41c0f14b83dd59232c2d2cccede43438314e
arstarkir <52779411+arstarkir@users.noreply.github.com>
2026-01-13 20:25:49 -0500

    Temp Lightning Visual + Handler (#20)

---
+++ Assets/_Scripts/Art/VFX/LightningHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class LightningHandler : MonoBehaviour
+{
+    [SerializeField] GameObject lightningVFX;
+    [SerializeField] List<ParticleSystem> particles = new List<ParticleSystem>();
+    [SerializeField] ParticleSystem warningLights;
+    [SerializeField] GameObject trigger;
+
+    private void Start()
+    {
+        lightningVFX.SetActive(false);
+        StartLightning(5);
+    }
+
+    public void StartLightning(float delay, float dmgTimeWindow = 0.2f)
+    {
+        foreach (ParticleSystem particle in particles)
+        {
+            ParticleSystem.MainModule main = particle.main;
+            main.startDelay = delay;
+        }
+
+        ParticleSystem.MainModule warningMain = warningLights.main;
+        warningMain.duration = delay;
+        lightningVFX.SetActive(true);
+        StartCoroutine(EnableDmgTrigger(delay, dmgTimeWindow));
+    }
+
+    IEnumerator EnableDmgTrigger(float delay, float dmgTimeWindow = 0.2f)
+    {
+        yield return new WaitForSeconds(delay);
+        if(trigger != null)
+            trigger.GetComponent<DealDamage>().enabled = true;
+        yield return new WaitForSeconds(dmgTimeWindow);
+        if (trigger != null)
+            trigger.GetComponent<DealDamage>().enabled = false;
+        Destroy(gameObject);
+    }
+}
