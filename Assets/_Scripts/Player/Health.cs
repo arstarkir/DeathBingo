173fb346a8a8faa5d28af6a9eed3c56f7df72e33
Monafide <wes.horne3@gmail.com>
2026-01-15 15:30:34 -0500

    Respawning Mechanic
    - respawn mechanic
    - invincibility time is seperate from death pool time, can be tweaked

--- Assets/_Scripts/Player/Health.cs
+++ Assets/_Scripts/Player/Health.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,10 @@ public class Health : Singleton<Health>
     List<GameObject> curHealth = new List<GameObject>();
     Coroutine damagePoolTimer = null;
     List<(DamageSource, IAttackHandler)> damageInfoPool = new List<(DamageSource, IAttackHandler)>();
+
+    public float respawnTimer = 3f; // seconds before respawn on death
+    [SerializeField] GameObject playerCollider; // collider to disable while dead
+    public float invincibilityTime = 3f;   // seconds of invincibiilty on respawn
 
     [Tooltip("Player will be damaged only once in that time frame. But other damages will go to the rules")]
     public float damagePoolTime = 1;
@@ -26,7 +31,7 @@ public class Health : Singleton<Health>
         {
             health += changeAmount;
             OnHealthChange();
-            damagePoolTimer = StartCoroutine(DamageDealtPool());
+            damagePoolTimer = StartCoroutine(DamageDealtPool(damagePoolTime));
         }
         damageInfoPool.Add((damageSource, handler));
         BingoController.instance.RuleCheck(damageInfoPool);
@@ -42,12 +47,37 @@ public class Health : Singleton<Health>
 
         for (int i = 0; i < health; i++)
             curHealth.Add(Instantiate(hpPref, hpHolder.transform));
+        if (health <= 0)
+        {
+            Die();
+        }
     }
 
-    IEnumerator DamageDealtPool()
+    IEnumerator DamageDealtPool(float duration)
     {
         yield return new WaitForSeconds(damagePoolTime);
         damageInfoPool.Clear();
         damagePoolTimer = null;
+    }
+
+    // hide, reset, and start couroutine for respawn when player dies
+    public void Die()
+    {
+        Rigidbody rb = playerCollider.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        playerCollider.SetActive(false);
+        StartCoroutine(Respawn());
+    }
+
+    // routine for waiting to respawn and respawning
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTimer);
+        playerCollider.SetActive(true);
+        playerCollider.transform.position = Vector3.zero;
+        health = 3;
+        OnHealthChange();
+        damagePoolTimer = StartCoroutine(DamageDealtPool(invincibilityTime));
     }
 }
