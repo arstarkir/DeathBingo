f0c6ade484040f89dcf15a1393e014559f527d43
arstarkir <52779411+arstarkir@users.noreply.github.com>
2026-01-15 17:41:25 -0500

    TentacleAttack01 + ToPlayer (#76 + #77)

--- Assets/_Scripts/Enemies/Attacks/ToPlayerAttack.cs
+++ Assets/_Scripts/Enemies/Attacks/ToPlayerAttack.cs
@@ -2,38 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// That's what happans when you rush (refering to the cahnges)
 public class ToPlayerAttack : MonoBehaviour, IAttackHandler
 {
     public AttackSO attackSO { get; set; }
 
-    readonly Queue<PastPlayerPos> samples = new Queue<PastPlayerPos>();
+    readonly Queue<TimePlayerPos> playerPoses = new Queue<TimePlayerPos>();
+    CharacterController cc;
 
     void Start()
     {
-        StartCoroutine(EndAfterTime(attackSO.duration, attackSO));
+        cc = CharacterController.instance;
     }
 
     void Update()
     {
-        samples.Enqueue(new PastPlayerPos(Time.time, CharacterController.instance.transform.position)); 
-        float targetTime = Time.time - Mathf.Max(0f, attackSO.playerPosDelay);
-        while (samples.Count > 1 && samples.Peek().t < targetTime)
-            samples.Dequeue();
-        this.transform.position = samples.Peek().pos;
-    }
+        playerPoses.Enqueue(new TimePlayerPos(Time.time, new Vector3(cc.transform.position.x,0, cc.transform.position.z))); 
 
-    public IEnumerator EndAfterTime(float s, AttackSO attack)
-    {
-        yield return new WaitForSeconds(s);
-        attack.EndAttack();
+        float targetTime = Time.time - Mathf.Max(0f, attackSO.playerPosDelay);
+        while (playerPoses.Count > 1 && playerPoses.Peek().t < targetTime)
+            playerPoses.Dequeue();
+
+        this.transform.position = playerPoses.Peek().pos;
     }
 }
 
-public struct PastPlayerPos
+public struct TimePlayerPos
 {
     public float t;
     public Vector3 pos;
-    public PastPlayerPos(float t, Vector3 pos)
+
+    public TimePlayerPos(float t, Vector3 pos)
     {
         this.t = t;
         this.pos = pos;
