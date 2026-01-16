54d69f73ca16c38252ef0f424638d65512962b4e
Monafide <wes.horne3@gmail.com>
2026-01-16 18:51:00 -0500

    LocationSO + Left Half Rule

---
+++ Assets/_Scripts/Rules/LocationRuleSO.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Location", menuName = "Rule/Location")]
+public class LocationRuleSO : DamageRuleSO
+{
+    [Header("Field spans (-10,0,-10) to (10,inf,10)")]
+    public Vector3 MinBounds = new Vector3(-10, 0, -10);
+    public Vector3 MaxBounds = new Vector3(10, 10, 10);
+    public override bool CheckRule((DamageSource, IAttackHandler) source)
+    {
+        if (trigger != source.Item1 && trigger != DamageSource.Ignore)
+            return false;
+
+        if (attackSO != null && (source.Item2 == null || source.Item2.attackSO.attackPref.name != attackSO.attackPref.name))
+            return false;
+
+        Vector3 playerPosition = CharacterController.instance.GetComponent<Collider>().bounds.center;
+        if (playerPosition.x < MinBounds.x || playerPosition.y < MinBounds.y || playerPosition.z < MinBounds.z)
+            return false;
+        if (playerPosition.x > MaxBounds.x || playerPosition.y > MaxBounds.y || playerPosition.z > MaxBounds.z)
+            return false;
+
+        return true;
+    }
+}
